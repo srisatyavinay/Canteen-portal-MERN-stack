a28a790f54d1208a5d6a22e1144cdda7b5cefc73
Venneti Sri Satya Vinay <81536213+srisatyavinay@users.noreply.github.com>
2022-02-15 23:39:39 +0530

    Add Docker

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connection to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/' + DB_NAME, { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://mongodb:27017/' + DB_NAME, { useNewUrlParser: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully !");
